a9ddd83be06fcbec1c2d0065a07677e85f9f5ce4
Eden Zik <edenzik@ZIKfam.com>
2015-05-14 14:59:21 -0400

    added sound script

---
+++ default_asoundrc_script.sh
@@ -0,0 +1,35 @@
+pcm.office {
+		type hw
+		card 1
+		device 0
+}
+
+pcm.itamar-room {
+		type hw
+		card 2
+		device 0
+}
+
+pcm.master-bedroom {
+		type hw
+		card 3
+		device 0
+}
+
+pcm.eden-room {
+		type hw
+		card 4
+		device 0
+}
+
+pcm.living-room {
+		type hw
+		card 5
+		device 0
+}
+
+pcm.kitchen {
+		type hw
+		card 6
+		device 0
+}
